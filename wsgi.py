65a30b835bd02c0b03c9a04fc1f0da104c5ff06f
Ehm User <martin.solberg@gmail.com>
2018-09-18 15:28:58 +0200

    init

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "OpenShift moramora!!"
+    return "OpenShift moramoramora!!"
 
 if __name__ == "__main__":
     application.run()
