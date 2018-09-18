1e6074137882e9a1dc51ed2279f6479553485440
Ehm User <martin.solberg@gmail.com>
2018-09-18 21:02:08 +0200

    restful

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,28 @@
+#from flask import Flask
+#from flask_restful import Resource, Api
+
+#application = Flask(__name__)
+
+#@application.route("/")
+#def hello():
+#    return "OpenShift moramoramora!!"
+
+#if __name__ == "__main__":
+#    application.run()
+
+
+# app.py - a minimal flask api using flask_restful
 from flask import Flask
 from flask_restful import Resource, Api
 
-application = Flask(__name__)
+app = Flask(__name__)
+api = Api(app)
 
-@application.route("/")
-def hello():
-    return "OpenShift moramoramora!!"
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-if __name__ == "__main__":
-    application.run()
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
